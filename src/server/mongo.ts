bc0913aceb3a445084abcfe0f5d1ea157b1805f3
wajdi-amer <wajdi.amerrr@gmail.com>
2022-07-16 02:12:51 +0300

    started to work on pokemons fusion

--- src/server/mongo.ts
+++ src/server/mongo.ts
@@ -1,5 +1,5 @@
 import { MongoClient, Db, Collection/*, WithId*/ } from 'mongodb';
-// import { Pokemon } from 'src/client/tsc/data';
+import { Pokemon } from 'src/client/tsc/data';
 
 export function create() {
   const uri = "mongodb://127.0.0.1:27017";
@@ -27,4 +27,27 @@ export async function getRandom(collection: Collection) {
   const listOfPokemon = await collection.find({}).toArray();
  const randomNumber = Math.floor(Math.random() * listOfPokemon.length)
  return listOfPokemon[randomNumber];
+}
+
+async function generateFusedPokemons(arr: any, collection: Collection) {
+  for (let i = 0 ; i < arr.length ; i++) {
+    for (let j = 0 ; j < arr.length ; j++ ){
+      if (i !== j) {
+       const fusedPokemon = await fusePokemons(arr[i], arr[j], collection);
+       await collection.insertOne(fusedPokemon);
+       console.log(fusedPokemon);
+      }
+    }
+  }
+}
+
+async function fusePokemons(pokemon1: Pokemon, pokemon2: Pokemon, collection: Collection) {
+  let newPokemon: Pokemon = {
+    id: (await collection.count({})) + 1,
+    name: pokemon1.name.slice(0, 4) + pokemon2.name.slice(3, 7),
+    height: Math.floor((parseInt(pokemon1.height) + parseInt(pokemon2.height)) / 2 ).toString(),
+    weight: Math.floor((parseInt(pokemon1.weight) + parseInt(pokemon2.weight)) / 2 ).toString(),
+    img: `https://raw.githubusercontent.com/Aegide/autogen-fusion-sprites/master/Battlers/${pokemon1.id}/${pokemon1.id}.${pokemon2.id}.png`
+  };
+  return newPokemon;
 }
